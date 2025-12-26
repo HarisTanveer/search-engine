6345bb02653691be53c17fb7f6782f08cac1603e
Haris Tanveer <haristanveer.ht@gmail.com>
2025-12-26 15:23:30 +0500

    updating search service to use overall word frequency from wordstat db

--- src/main/java/com/obvio/assignment/services/SearchService.java
+++ src/main/java/com/obvio/assignment/services/SearchService.java
@@ -1,30 +1,43 @@
 package com.obvio.assignment.services;
 
 import com.obvio.assignment.Repositories.FileDictionaryRepository;
+import com.obvio.assignment.Repositories.WordStatsRepository;
 import com.obvio.assignment.dto.SearchResponse;
 import com.obvio.assignment.models.FileDictionary;
+import com.obvio.assignment.models.WordStats;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SearchService {
-    private final FileDictionaryRepository  fileDictionaryRepository;
+    private static final Logger logger = LoggerFactory.getLogger(SearchService.class);
+    private final FileDictionaryRepository fileDictionaryRepository;
+    private final WordStatsRepository wordStatsRepository;
 
     public List<SearchResponse> searchWord(String word) {
         List<SearchResponse> results = new ArrayList<>();
         List<FileDictionary> list = fileDictionaryRepository.findByWord(word);
-        Integer totalDocumentsContainingWord = list.size();
+        Integer totalDocumentsContainingWord = 0;
+        Optional<WordStats> wordStats = wordStatsRepository.findByWord(word);
+        if (wordStats.isPresent()) {
+            totalDocumentsContainingWord = wordStats.get().getDocumentFrequency();
+        }
+        logger.info("Total documents containing the word {} :{}", word, totalDocumentsContainingWord);
         Integer overallTotalDocuments = fileDictionaryRepository.totalDocuments();
+        logger.info("Total documents in the search engine: {}", overallTotalDocuments);
         for (FileDictionary fileDictionary : list) {
             results.add(new SearchResponse(fileDictionary.getFileName(), calculateRanking(fileDictionary, overallTotalDocuments, totalDocumentsContainingWord)));
         }
 
-        results.sort(Comparator.comparing(SearchResponse::getRanking).thenComparing(SearchResponse::getDocumentName));
+        results.sort(Comparator.comparing(SearchResponse::getRanking).reversed().thenComparing(SearchResponse::getDocumentName));
         if(results.size() >= 3){
             return results.subList(0, 3);
         }
